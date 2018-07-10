650f96c4fa6d097da8a14c0db4a23eed22a960c2
rachel1032 <racheles32@gmail.com>
2018-07-10 15:42:47 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -5,6 +5,9 @@ end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on
 
+breakfast = #{name}
+
+
 
 
 
